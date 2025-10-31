7147726e92d5fcdb01d02399b7867e5847f9a0f8
Samuel Li <samuelgh.li@gmail.com>
2025-10-31 13:23:29 +1100

    setup design and layout

--- banner.js
+++ banner.js
@@ -1,1 +1,3 @@
 const frames=['frame frame-1','frame end-frame'];
+const tcA="*^T&Cs apply insurance issued by Auto & General Insurance Co Ltd.";
+const tcB="See the PDS and TMD at qantas.com.au/insurnace to decide if product suits.";
